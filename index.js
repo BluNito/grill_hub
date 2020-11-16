0ecbfd48114ba2b785e60f7885248ec5edbc99de
Mukul P <mukul.p.mohan@gmail.com>
2020-11-16 08:16:39 +0530

    fixed production keys path

--- index.js
+++ index.js
@@ -33,10 +33,17 @@ app.use("/api/orders", orders);
 
 //server routes in production
 if (process.env.NODE_ENV === "production") {
+  console.log("Running in production");
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
+} else {
+  console.log("Running in development");
+  // app.use(express.static("client/build"));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 }
 
 const port = process.env.PORT || 5000;
